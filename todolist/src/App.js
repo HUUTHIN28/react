01656d3b761d204b80268c7c0c0a56f6ed90de4d
huu thin <huuthin30@gmail.com>
2021-07-13 03:21:12 -0700

    them app.css app.js

--- todolist/src/App.js
+++ todolist/src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './App.css';
+import React from 'react';
+class App extends React.Component{
+constructor(props){
+  super(props);
+  this.state = {taskname: ' ',tasks : []};
+}
+myChang = (event) => {
+  this.setState({taskname: event.target.value});
 }
 
+shoot = () => {
+  console.log('thin', this.state.taskname);
+  this.state.tasks.push(this.state.tasknamen );
+  //this.setState({tasks: newtasks});
+ this.setState({taskname:' '});
+}
+render(){
+  return (
+   <div className="App">
+     <header className ="App-header">
+       <div className="alinged">
+         <br/>
+         to do list : {this.state.taskname}
+         <br/>
+     <img src="https://cdn1.iconfinder.com/data/icons/office-293/128/_Add_new_plus_-128.png" 
+     alt="cộng"
+      height='20' 
+      width="30" 
+      style={{ cursor:'pointer' }}
+      title='bấm vào day'
+      onClick={() => this.shoot()}
+      />
+       <input type="text" width="30"
+       value={this.state.taskname}
+       onChange={this.myChang}
+       />
+         <ul>
+           {this.state.tasks.map((value, index) =>{
+             return <li key={index} className="task">{value}</li>
+           })}
+         </ul>
+       </div>
+     </header>
+   </div>
+  );
+  }
+  }
 export default App;
